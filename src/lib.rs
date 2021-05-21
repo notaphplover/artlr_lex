cfdb0bb3899d20e594554f39bb78b702fe61109e
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2021-05-21 18:56:02 +0200

    feat(text_location): add TextLocation

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,2 @@
+pub mod text_location;
 pub mod token;
---
+++ src/text_location.rs
@@ -0,0 +1,26 @@
+pub struct TextLocation {
+    pub column: u64,
+    pub line: u64,
+}
+
+impl TextLocation {
+    pub fn new(column: u64, line: u64) -> Self {
+        TextLocation { column, line }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::text_location::TextLocation;
+
+    #[test]
+    fn new_returns_instance() {
+        const COLUMN: u64 = 2;
+        const LINE: u64 = 5;
+
+        let text_location: TextLocation = TextLocation::new(COLUMN, LINE);
+
+        assert_eq!(text_location.column, COLUMN);
+        assert_eq!(text_location.line, LINE);
+    }
+}
