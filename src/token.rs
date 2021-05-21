37a6251c163f31e774acf70f3c951247efafe153
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2021-05-21 18:55:25 +0200

    feat(token): add Token

---
+++ src/token.rs
@@ -0,0 +1,26 @@
+pub struct Token<'a, TType> {
+    pub t_type: TType,
+    pub text: &'a str,
+}
+
+impl<'a, TType> Token<'a, TType> {
+    pub fn new(t_type: TType, text: &'a str) -> Self {
+        Token { t_type, text }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::token::Token;
+
+    #[test]
+    fn new_returns_instance() {
+        const TOKEN_TYPE: &str = "sample_type";
+        const TOKEN_TEXT: &str = "sample_text";
+
+        let token: Token<&str> = Token::new(TOKEN_TYPE, TOKEN_TEXT);
+
+        assert_eq!(token.t_type, TOKEN_TYPE);
+        assert_eq!(token.text, TOKEN_TEXT);
+    }
+}
