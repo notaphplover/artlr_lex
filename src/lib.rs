e90aa85362e1c16653e27958504d698075b2b30e
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2021-05-21 18:56:46 +0200

    feat(token): add TraceableToken

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,3 @@
 pub mod text_location;
 pub mod token;
+pub mod traceable_token;
---
+++ src/traceable_token.rs
@@ -0,0 +1,13 @@
+use crate::token::Token;
+use crate::text_location::TextLocation;
+
+pub struct TraceableToken<'a, TType> {
+    pub location: TextLocation,
+    pub token: Token<'a, TType>,
+}
+
+impl<'a, TType> TraceableToken<'a, TType> {
+    pub fn new(location: TextLocation, token: Token<'a, TType>) -> Self {
+        TraceableToken { location, token }
+    }
+}
